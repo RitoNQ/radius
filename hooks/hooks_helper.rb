5c745ee34e312f26f77093a1c70050b1083c3d18
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2020-10-21 18:26:52 +0000

    ALSA: control: Add sanity checks for user ctl id name string

--- hooks/hooks_helper.rb
+++ hooks/hooks_helper.rb
@@ -7,3 +7,5 @@ require File.expand_path('../../lib/merge_message_modifier', __FILE__)
 require File.expand_path('../../lib/emoji_descriptor', __FILE__)
 
 # Finds the name of the current branch for repo.
+#
+# Returns a String.
