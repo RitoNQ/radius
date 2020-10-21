580ff5195e34dc3f06100468d887a7b77104693b
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2020-10-21 15:46:52 +0000

    UPDATE changelog

--- hooks/hooks_helper.rb
+++ hooks/hooks_helper.rb
@@ -3,3 +3,5 @@ require "rugged"
 require "gemoji"
 
 require File.expand_path('../../lib/description_prepender', __FILE__)
+require File.expand_path('../../lib/merge_message_modifier', __FILE__)
+require File.expand_path('../../lib/emoji_descriptor', __FILE__)
