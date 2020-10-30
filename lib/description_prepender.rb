a1a0c9d42f485e8fd977e8a24be240c7b4d9c2de
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2020-10-30 13:56:51 +0000

    trying terminal_print_frame as primary repl output

--- lib/description_prepender.rb
+++ lib/description_prepender.rb
@@ -2,3 +2,4 @@
 # Prepends the branch description to each commit message, after the commit message has been set.
 #
 # Add Branch description with git branch --edit-description branch-name
+class DescriptionPrepender
