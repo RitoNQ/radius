c75b4759316b2d7511a4d9ed870fa7b1b1033c7d
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2021-02-27 07:29:24 +0000

    corrected link

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -1,4 +1,4 @@
 # If there is no description set for the current branch, choose
 # a random GitHub emoji code to use as the description.
 #
-# This can always be overwritte
+# This can always be overwritten
