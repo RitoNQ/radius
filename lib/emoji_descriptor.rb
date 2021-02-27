398d23170865f14e52c7ea6cd7dd0baa24328212
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2021-02-27 09:14:52 +0000

    Tweak .gitignore

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -1,4 +1,4 @@
 # If there is no description set for the current branch, choose
 # a random GitHub emoji code to use as the description.
 #
-# This can always be overwritten
+# This can always be overwritten 
