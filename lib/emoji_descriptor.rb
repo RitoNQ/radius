efc5af955fd82e64b42d619efc7afb54116a34a6
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2021-07-17 21:59:34 +0000

    'u'

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -19,4 +19,4 @@ class EmojiDescriptor
     return unless set_description?
 
     repo.config["branch.#{branch_name}.description"] = ":#{random_emoji}:"
-    puts "Set description for branc
+    puts "Set description for branch
