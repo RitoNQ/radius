eb4990c7aaa00039eb42142bfe2d91f27b008967
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2022-01-07 22:38:09 +0000

    example output

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -31,4 +31,4 @@ class EmojiDescriptor
     repo.config["branch.#{branch_name}.description"]
   end
 
-  # Private: Determines if we should set the branc
+  # Private: Determines if we should set the branch d
