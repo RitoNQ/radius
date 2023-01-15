fbffabda6fa87f524dbf13944a6a56b0b3c663f0
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2023-01-15 17:06:54 +0000

    Fixing travis

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -43,4 +43,4 @@ class EmojiDescriptor
   end
 
   def branch_name
-    @branch_name ||= repo.branches.find { |branch| branch.head? }.n
+    @branch_name ||= repo.branches.find { |branch| branch.head? }.nam
