8d755ae6b41334fa6facb4a57d4bb88b406890a5
RitoNQ <90030036+RitoNQ@users.noreply.github.com>
2023-01-15 20:40:31 +0000

    Thunderbird aktualisiert

--- lib/emoji_descriptor.rb
+++ lib/emoji_descriptor.rb
@@ -43,4 +43,4 @@ class EmojiDescriptor
   end
 
   def branch_name
-    @branch_name ||= repo.branches.find { |branch| branch.head? }.nam
+    @branch_name ||= repo.branches.find { |branch| branch.head? }.name
